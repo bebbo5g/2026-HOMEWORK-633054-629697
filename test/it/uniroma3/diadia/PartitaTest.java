ddaacc892f9bc76f2a984de56078b6db8539ab87
Umberto Di Lallo <umby.dilallo@gmail.com>
2026-04-02 17:51:31 +0200

    Test Labirinto

--- test/it/uniroma3/diadia/PartitaTest.java
+++ test/it/uniroma3/diadia/PartitaTest.java
@@ -1,53 +1,50 @@
 package it.uniroma3.diadia;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PartitaTest {
-	
+
 	private Partita partita;
-	
+
 	@BeforeEach
-	public void setUp()
-	{
-        partita = new Partita();
+	public void setUp() {
+		partita = new Partita();
 	}
-	
+
 	@Test
 	public void testPartitaNonVinta() {
 		assertFalse(partita.vinta());
 	}
-	
+
 	@Test
-	public void testPartitaNonFinitaAllInizio(){
+	public void testPartitaNonFinitaAllInizio() {
 		assertFalse(partita.isFinita());
 	}
-	
+
 	@Test
 	public void testSetFinita() {
 		partita.setFinita();
 		assertTrue(partita.isFinita());
 	}
-	
+
 	@Test
-	public void testPartitaFinitaQuandoCfuZero()
-	{
-		partita.giocatore.setCfu(0);
+	public void testPartitaFinitaQuandoCfuZero() {
+		partita.getGiocatore().setCfu(0);
 		assertTrue(partita.isFinita());
 	}
-	
+
 	@Test
-	public void testPartitaVintaStanzaVincente()
-	{
-		partita.labirinto.setStanzaCorrente(partita.labirinto.getStanzaVincente());
+	public void testPartitaVintaStanzacaVincente() {
+		partita.setStanzaCorrente();
 		assertTrue(partita.vinta());
 	}
-	
+
 	@Test
-	public void testPartitaFinitaStanzaVincente()
-	{
+	public void testPartitaFinitaStanzaVincente() {
 		partita.labirinto.setStanzaCorrente(partita.labirinto.getStanzaVincente());
 		assertTrue(partita.isFinita());
 	}
