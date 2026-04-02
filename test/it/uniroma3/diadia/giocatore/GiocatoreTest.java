e4af2b9dbc0cbdf7ce9e98ea4cbd6b119d410d5e
Luca De Santis <lucaroma2705@gmail.com>
2026-04-02 17:43:25 +0200

    test classe Giocatore

--- test/it/uniroma3/diadia/giocatore/GiocatoreTest.java
+++ test/it/uniroma3/diadia/giocatore/GiocatoreTest.java
@@ -1,22 +1,24 @@
 package it.uniroma3.diadia.giocatore;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GiocatoreTest {
-	
-	private Giocatore giocatore;
-	@BeforeEach
-	public void setUp(){
-		giocatore = new Giocatore();
+
+	@Test
+	public void testCfuIniziali() {
+		Giocatore giocatore = new Giocatore();
+
+		assertEquals(giocatore.getCfu(), 20);
 	}
 
 	@Test
-	public void testSetGetCfu() {
-		giocatore.setCfu(10);
-		assertEquals(giocatore.getCfu(), 10);
-	}
+	public void testSetCfu() {
+		Giocatore giocatore = new Giocatore();
 
+		giocatore.setCfu(14);
+
+		assertEquals(giocatore.getCfu(), 14);
+	}
 }
